6ef00d59ca0e3b81bbea75b0beb3b6f744c129b1
atikah2301 <atikah.hussain@hotmail.co.uk>
2024-02-17 22:05:01 +0000

    Add multi line seed data

--- tutor_app/app.py
+++ tutor_app/app.py
@@ -1,11 +1,15 @@
+import csv
 import logging
+import os
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-logging.basicConfig(level=logging.INFO)
+
+LOG_PATH = 'app.log'
+logging.basicConfig(level=logging.INFO, filename=LOG_PATH, filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 DATABASE_URL = "sqlite:///database.db"
 engine = create_engine(DATABASE_URL)
@@ -27,12 +31,20 @@ class Tutor(db.Model):
         return "<Tutor %r>" % self.tutor_id
 
 
-def initialise_database():
+def seed_database():
+    # Delete and recreate all the tables
     db.drop_all()
-    db.create_all()  # Recreate all the tables
-    tutor_1 = Tutor(tutor_id=1234, name="John Doe", email="john.doe@tutorplanet.co.uk")
-    db.session.add(tutor_1)
-    db.session.commit()
+    db.create_all()
+    
+    seed_data = [
+        ["1234", "John Doe", "john.doe@tutorplanet.co.uk"], 
+        ["1245", "Jane Smith", "jane.smith@tutorplanet.co.uk"],
+    ]
+    for row in seed_data:
+        tutor = Tutor(tutor_id=int(row[0]), name=row[1], email=row[2])
+        logging.info(f"Seed data {tutor} added")
+        db.session.add(tutor)
+        db.session.commit()
 
 
 @app.route("/")
@@ -56,9 +68,9 @@ def tutor_profile(tutor_id):
 
 
 with app.app_context():
-    initialise_database()
-
+    seed_database()
 
 if __name__ == "__main__":
-    app.run(debug=True)
     logging.info("Running flask app...")
+    app.run(debug=False)
+    
