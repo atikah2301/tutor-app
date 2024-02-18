881d25001523b7bc18a09329dfcf138043ea3dfd
atikah2301 <atikah.hussain@hotmail.co.uk>
2024-02-18 12:36:30 +0000

    Improve existing tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,8 @@
 import pytest
 
+from tutor_app.app import Tutor, seed_database
 from tutor_app.app import app as flask_app
+from tutor_app.app import db
 
 
 @pytest.fixture()
@@ -10,15 +12,18 @@ def app():
             "TESTING": True,
         }
     )
-
-    # other setup can go here
     yield flask_app
-    # clean up / reset resources here
 
 
-@pytest.fixture()
-def client(app):
-    return app.test_client()
+@pytest.fixture
+def client():
+    flask_app.config['TESTING'] = True
+    with flask_app.test_client() as client:
+        with flask_app.app_context():
+            seed_database()
+        yield client
+        with flask_app.app_context():
+            db.drop_all()
 
 
 @pytest.fixture()
@@ -26,6 +31,63 @@ def runner(app):
     return app.test_cli_runner()
 
 
-def test_request_example(client):
+def test_homepage(client):
     response = client.get("/")
     assert response.status_code == 200
+    assert b"<h1>Tutor Planet</h1>" in response.data
+    assert b"<h2>Welcome to Tutor Planet!</h2>" in response.data
+
+
+def test_browse_tutors(client):
+    response = client.get("/browse-tutors")
+    assert response.status_code == 200
+
+
+def test_tutor_signup_get(client):
+    response = client.get("/tutor-signup")
+    assert b'<form id="tutor-signup-form">' in response.data
+    assert b'<button type="submit">Sign Up</button>' in response.data
+    assert response.status_code == 200
+
+
+def test_tutor_signup_post_success(client):
+    response = client.post(
+        "/tutor-signup",
+        data={
+            "name": "Samantha Doe",
+            "email": "samantha@example.com",
+            "password": "my_password",
+        },
+    )
+    assert response.status_code == 200
+    assert Tutor.query.filter_by(name="Samantha Doe").first() is not None
+    assert response.json["message"] == "You've successfully signed up as a tutor!"
+
+
+def test_tutor_signup_post_failure_incomplete_form(client):
+    response = client.post(
+        "/tutor-signup",
+        data={
+            "email": "samantha@example.com",
+            "password": "my_password",
+        },
+        # Note that in practice the html is smart enough to not allow you to submit the form 
+        # without completing all the sections
+    )
+    assert response.status_code == 400
+    assert Tutor.query.filter_by(email="samantha@example.com").first() is None
+    assert response.json is None
+
+
+def test_tutor_signup_post_failure_email_taken(client):
+    response = client.post(
+        "/tutor-signup",
+        data={
+            "Johnny Doe"
+            "email": "john.doe@tutorplanet.com",
+            "password": "my_password",
+        },
+    )
+    assert response.status_code == 400
+    assert Tutor.query.filter_by(email="john.doe@tutorplanet.com").first() is None
+    assert response.json is None
--- tutor_app/app.py
+++ tutor_app/app.py
@@ -1,9 +1,9 @@
 import logging
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker
 
 LOG_PATH = "app.log"
@@ -35,6 +35,55 @@ class Tutor(db.Model):
         return "<Tutor %r>" % self.tutor_id
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/browse-tutors")
+def browse_tutors():
+    tutors = Tutor.query.all()
+    return render_template("browse-tutors.html", tutors=tutors)
+
+
+@app.route("/tutor-<int:tutor_id>-profile")
+def tutor_profile(tutor_id):
+    tutor = Tutor.query.get_or_404(tutor_id)
+    return render_template("tutor-profile.html", tutor=tutor)
+
+
+@app.route("/tutor-signup", methods=["GET", "POST"])
+def tutor_signup():
+    if request.method == "GET":
+        return render_template("tutor-signup.html")
+
+    elif request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        try:
+            tutor = Tutor(name=name, email=email, password=password)
+            db.session.add(tutor)
+            db.session.commit()
+
+            return jsonify(message="You've successfully signed up as a tutor!")
+
+        except IntegrityError as e:
+            db.session.rollback()
+
+            return jsonify(
+                message="The email you have entered is already in use for an existing tutor account."
+            )
+
+        except Exception as e:
+            db.session.rollback()
+
+            return jsonify(
+                message="An error occurred while signing up. Please try again later."
+            )
+
+
 def seed_database():
     # Delete and recreate all the tables
     db.drop_all()
@@ -49,47 +98,6 @@ def seed_database():
         logging.info(f"Seed data {tutor} added")
         db.session.add(tutor)
         db.session.commit()
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/browse-tutors")
-def browse_tutors():
-    session = Session()
-    tutors = Tutor.query.all()
-    session.close()
-
-    return render_template("browse-tutors.html", tutors=tutors)
-
-
-@app.route("/tutor-<int:tutor_id>-profile")
-def tutor_profile(tutor_id):
-    tutor = Tutor.query.get_or_404(tutor_id)
-    return render_template("tutor-profile.html", tutor=tutor)
-
-
-@app.route("/tutor-signup", methods=['GET', 'POST'])
-def tutor_signup():
-    if request.method == "GET":
-        return render_template("tutor-signup.html")
-
-    elif request.method == "POST":
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
-
-        try:
-            tutor = Tutor(name=name, email=email, password=password)
-            db.session.add(tutor)
-            db.session.commit()
-            return jsonify({"message": "You've successfully signed up as a tutor"})
-        
-        except SQLAlchemyError as e:
-            db.session.rollback()
-            return jsonify({"message": "An error occurred while signing up. Please try again later."})
 
 
 with app.app_context():
